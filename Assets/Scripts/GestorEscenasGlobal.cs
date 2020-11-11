deb90e10c9abf845f288dcadda597c07b3513806
renedominguezpalenzuela <renedp1975@gmail.com>
2020-11-11 08:25:57 -0400

    movimientos de camara

---
+++ Assets/Scripts/GestorEscenasGlobal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine.SceneManagement;
+
+
+public class GestorEscenasGlobal : MonoBehaviour
+{
+    public static GestorEscenasGlobal Instance{ set; get;}
+    //public string NombreEscena { get => nombreEscena; set => nombreEscena = value; }
+
+    //private string nombreEscena;
+
+
+    //----------------------------------------------------------------- 
+    //  Cargar escenas iniciales, splash, menu, etc…
+    //----------------------------------------------------------------- 
+
+    private void Awake(){
+        Instance = this;
+          
+        Load("00_Splash");
+        // GestorEscenasGlobal.Instance.Load ("SampleScene");
+    }
+
+
+    //----------------------------------------------------------
+    // Load a scene (Additive)
+    //----------------------------------------------------------
+    // Parameters:
+    // sceneName -- Scene Name
+    public void Load(string sceneName){
+        if (!SceneManager.GetSceneByName (sceneName).isLoaded) {
+            SceneManager.LoadScene (sceneName, LoadSceneMode.Additive);
+        }        
+    }
+
+    public void Unload(string sceneName){
+        if (SceneManager.GetSceneByName(sceneName).isLoaded) {         
+            SceneManager.UnloadSceneAsync (sceneName);
+        }
+    }
+
+
+
+}
