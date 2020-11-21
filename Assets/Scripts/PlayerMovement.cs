4ba6458926a5c06fde51c5ad7b81841a27c7f504
renedominguezpalenzuela <renedp1975@gmail.com>
2020-11-21 09:39:10 -0400

    Limitando movimientos de camara en la vertical

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -123,10 +123,10 @@ public class PlayerMovement : MonoBehaviour{
 
         //Demora
 
-         int a  = 0;
+       /*  int a  = 0;
         for (int i = 0; i<=100000000; i++){
            a = a^2; 
-        }
+        }*/
 
     }
 
--- Assets/Scripts/UI/MovimientoCamara.cs
+++ Assets/Scripts/UI/MovimientoCamara.cs
@@ -1,98 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class MovimientoCamara : MonoBehaviour
 {
-
-    /*[SerializeField] private Camera cam;
-    [SerializeField] private Transform target;
-    [SerializeField] private float distanceToTarget = 10;*/
-
     public Camera cam;
     public Transform target;
     public float distanceToTarget = 16.5f;
-    
-    
     private Vector3 previousPosition;
     private float rotationAroundXAxis = 0f;
     private float rotationAroundYAxis = 0f;
-    private float anguloCamara;
-
-
-    // float minRotation = -5;
-    // float maxRotation = 35;
     
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
-           // textoDebug.text = "Iniciando movimiento de Camara SWIPE";
 
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
-            Vector3 direction = previousPosition - newPosition;
-                     
-           
-            rotationAroundYAxis = -direction.x * 180; // camera moves horizontally                  
-            rotationAroundXAxis = direction.y * 180; // camera moves vertically
+    [SerializeField] private float MaxAnguloHaciaArriba = 30.0f;
+    [SerializeField] private float MaxAnguloHaciaAbajo = 2.0f;
 
-            
-           
+    
+    private float xAxisClamp;
 
-           
-             
-             // textoDebug.text = "Angulo Camara "+newPosition;
-
-                          
-              
-              
-            
-              cam.transform.position = target.position;
-
-              cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);                                                             
-              cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
-              cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
-              previousPosition = newPosition;
-            
-
-            
-          
-
-           
-            
-           
-        }
-
- 
+    private void Start() {
+        xAxisClamp = 0;
+        cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+       //NOTA: en el primer clic la camara se ajusta hacia abajo provocando un salto molesto
+       //Solucion: ubicar inicialmente la camara en la posicion ajustada hacia abajo
     }
 
-        
+    void Update()  {
+        if (Input.GetMouseButtonDown(0))  {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            // textoDebug.text = "Iniciando movimiento de Camara SWIPE";
+        }
+        else if (Input.GetMouseButton(0) ) {
 
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
 
-  public static float ConvertToAngle180(float input)
-     {       
-         while (input > 360)
-         {
-             input = input - 360;
-         } 
-         while (input < -360)
-         {
-             input = input + 360;
-         }
-         if (input > 180)
-         {
-             input = input - 360;        
-         }
-         if (input < -180)
-             input = 360+ input;
-         return input;
-     }
-   
+            
+            Vector3 direction = previousPosition - newPosition;
+            rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            //Limitar moviviemtto
+            
+            xAxisClamp += rotationAroundXAxis;
+            if (xAxisClamp >MaxAnguloHaciaArriba)   {
+                xAxisClamp = MaxAnguloHaciaArriba;
+                rotationAroundXAxis = 0.0f; //no rotar llego al limite             
+                ClampXAxisRotationToValue(MaxAnguloHaciaArriba); //Valores absolutos
+                Debug.Log("Hacia Arriba " + xAxisClamp+" "+rotationAroundXAxis);
+            }
+            else if (xAxisClamp <MaxAnguloHaciaAbajo)  {
+                xAxisClamp = MaxAnguloHaciaAbajo;
+                rotationAroundXAxis = 0.0f; //no rotar  llego al limite  
+                ClampXAxisRotationToValue(MaxAnguloHaciaAbajo);      
+                Debug.Log("Hacia Abajo " + xAxisClamp+" "+rotationAroundXAxis);
+            }
+            
+
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+            previousPosition = newPosition;
+           
+        }
+    }
+
+        private void ClampXAxisRotationToValue(float value) {
+        Vector3 eulerRotation = cam.transform.eulerAngles;
+        eulerRotation.x = value;
+        cam.transform.eulerAngles = eulerRotation;
+    }
 
 }
